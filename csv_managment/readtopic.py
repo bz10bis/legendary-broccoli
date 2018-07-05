0205c361c9503d08ff48705b32603dea387b3135
bz10bis <baptistezegre@gmail.com>
2018-07-05 14:46:12 +0200

    resolve http issue

--- csv_managment/readtopic.py
+++ csv_managment/readtopic.py
@@ -1,6 +1,6 @@
 from kafka import KafkaConsumer
-
+print("Connecting...")
 consumer = KafkaConsumer('test', bootstrap_servers=['10.33.1.131:29092'])
-
+print("Waiting for message")
 for msg in consumer:
     print(msg)
